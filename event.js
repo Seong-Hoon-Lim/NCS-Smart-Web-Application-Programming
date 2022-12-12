246559fea7a56ec698587d4eccf0f681598a5036
Seong-Hoon-Lim <108711069+Seong-Hoon-Lim@users.noreply.github.com>
2022-12-12 21:27:00 +0900

    스마트 웹 응용 프로그램 프로그래밍 국비지원 과정 Day 27

---
+++ event.js
@@ -0,0 +1,8 @@
+// document.addEventListener("DOMContentLoaded", ()=> {
+    const heading = document.querySelector("h1");
+    console.log(heading);
+    heading.style.background = "#73baa3";
+// })
+
+
+
