0d165a35fe3350b1339306b1149663f38e98813b
Seong-Hoon-Lim <108711069+Seong-Hoon-Lim@users.noreply.github.com>
2022-12-17 23:48:18 +0900

    스마트 웹 응용 프로그램 프로그래밍 국비지원 과정 Day 30

--- event.js
+++ event.js
@@ -1,8 +1,5 @@
-// document.addEventListener("DOMContentLoaded", ()=> {
-    const heading = document.querySelector("h1");
-    console.log(heading);
-    heading.style.background = "#73baa3";
+// document.addEventListener('DOMContentLoaded', ()=> {
+  const heading = document.querySelector('h1');
+  console.log(heading);
+  heading.style.background = "#73baa3";
 // })
-
-
-
