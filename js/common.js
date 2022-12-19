75c9621b6bc443acc4b5a90a6f36a49fb2d0d088
Seong-Hoon-Lim <108711069+Seong-Hoon-Lim@users.noreply.github.com>
2022-12-19 23:38:24 +0900

    스마트 웹 응용 프로그램 프로그래밍 국비지원 과정 포트폴리오

--- js/common.js
+++ js/common.js
@@ -1,6 +1,40 @@
+// 네비게이션 mouseover,mouseout
 const gnbList = document.querySelectorAll("#gnbList > li > a");
 const snb = document.querySelectorAll(".snb");
 
+gnbList.forEach( (gli, i)=>{
+    gli.addEventListener("mouseover", ()=>{
+        snb[i-1].style.opacity = "1";
+        setTimeout(()=> {
+        }, 500);
+    })
+    gli.addEventListener("mouseout", ()=> {
+        snb[i-1].style.opacity = "0";
+        setTimeout(()=> {
+        }, 500);
+    })
+})
 
-for (let i=0; i<gnbList.length; i++) {
-}
+// 슬라이더 버튼 click
+const sliderList = document.getElementById("sliderList");
+const sliderBtn = document.querySelectorAll(".sliderBtn a");
+const eventBtnWrap = document.querySelectorAll(".eventBtnWrap");
+
+
+sliderBtn[0].addEventListener("click", ()=> {
+    sliderList.style.transform = `translateX(0%)`;   
+    sliderList.style.transition = "0.5s";
+    eventBtnWrap[0].style.zIndex = 999;
+
+    
+})
+sliderBtn[1].addEventListener("click", ()=> {
+    sliderList.style.transform = `translateX(-33.29%)`;   
+    sliderList.style.transition = "0.5s";  
+    
+})
+sliderBtn[2].addEventListener("click", ()=> {
+    sliderList.style.transform = `translateX(-66.58%)`; 
+    sliderList.style.transition = "0.5s";  
+    
+})
