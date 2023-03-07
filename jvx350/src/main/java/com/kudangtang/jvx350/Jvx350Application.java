4941cb85e1dd5034489506c55121a3e143b05972
Seong-Hoon-Lim <108711069+Seong-Hoon-Lim@users.noreply.github.com>
2023-03-07 13:45:57 +0900

    (NCS)국비지원교육 : 스마트웹 응용 프로그래밍 과정
    Java 응용 프로그래밍 / 웹 프로그래밍 (Servlet, JSP, SpringFrameWork, SpringBoot)

---
+++ jvx350/src/main/java/com/kudangtang/jvx350/Jvx350Application.java
@@ -0,0 +1,14 @@
+package com.kudangtang.jvx350;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jvx350Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jvx350Application.class, args);
+		
+	}
+
+}
